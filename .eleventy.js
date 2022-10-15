5000602144b8ec6834bea0dc62da6b10513352d9
Ilya Siganov <ilya.blan4@gmail.com>
2022-10-16 00:31:09 +0600

    add list of talks and notes to main page

--- .eleventy.js
+++ .eleventy.js
@@ -19,6 +19,16 @@ module.exports = function (eleventyConfig) {
       "dd LLL yyyy"
     );
   });
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if (n < 0) {
+      return array.slice(n);
+    }
+    return array.slice(0, n);
+  });
 
   return {
     templateFormats: ["md", "njk", "html"],
